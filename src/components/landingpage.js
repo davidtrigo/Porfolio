4dfe91af1bf1ed8a4c23cae7d84be23f5c177870
davidtrigo <d.trigo83@gmail.com>
2020-11-18 01:25:00 +0100

    edit ladingpage and adding class in App.css

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return (
-            <    div><h1>Landing page</h1></div>
+            <div style={{width: '100%', margin:'auto'}}>
+            <Grid className="landing-grid">
+                <Cell col={12}>
+                    
+                  <img
+                    src="https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png"
+                    alt="avatar"
+                    className="avatar-image"
+                  />
+                  <div className="banner-text">
+                    <h1>Full Stack Web Developer</h1>
+                    <hr/>
+                    <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoBD </p>
+                      
+                      <div className="social-links"> 
+                        {/*LinkedIn*/}
+                        <a href="http://linkedin.com" target="_blank" rel="noopener noreferrer" >
+                            <i className="fa fa-linkedin-square" aria-hidden="true"/> 
+                        </a>
+                        {/*Github*/}
+                        <a href="http://github.com" target="_blank" rel="noopener noreferrer" >
+                            <i className="fa fa-github-square" aria-hidden="true"/> 
+                        </a>
+
+                        {/*Free Code camp*/}
+                        <a href="https://www.freecodecamp.org/" target="_blank" rel="noopener noreferrer" >
+                            <i className="fa fa-free-code-camp" aria-hidden="true"/> 
+                        </a>
+                        </div>
+                  
+                  </div>
+              
+                </Cell>
+            </Grid>
+            </div>
         );
     }
 
