7ee235b8b4e3d08a04ef992f535051cad4380f34
davidtrigo <d.trigo83@gmail.com>
2020-11-18 21:01:31 +0100

    Using  image in src/img  instead of url remote

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import avatar from '../img/avatar.png';
 
 class Landing extends Component {
     render() {
@@ -7,12 +8,16 @@ class Landing extends Component {
             <div style={{width: '100%', margin:'auto'}}>
             <Grid className="landing-grid">
                 <Cell col={12}>
-                    
-                  <img
+              { /* <img
                     src="https://cdn0.iconfinder.com/data/icons/avatars-6/500/Avatar_boy_man_people_account_client_male_person_user_work_sport_beard_team_glasses-512.png"
                     alt="avatar"
                     className="avatar-image"
-                  />
+              /> */ }          
+           
+
+                  <img  src={avatar} alt="avatar" className="avatar-image" />
+
+
                   <div className="banner-text">
                     <h1>Full Stack Web Developer</h1>
                     <hr/>
