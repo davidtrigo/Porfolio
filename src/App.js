a9406500f4e558f08281dd67c038f2b71a06a7d2
davidtrigo <d.trigo83@gmail.com>
2020-11-18 01:29:25 +0100

    using NavegationBar

--- src/App.js
+++ src/App.js
@@ -2,27 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import { Content, Drawer, Header, Layout, Navigation } from 'react-mdl';
 import Main from './components/main';
-import { Link } from 'react-router-dom';
 import NavegationBar from './components/navegationBar';
 class App extends Component {
   render() {
     return (
       <div className="demo-big-content">
         <Layout>
-          <Header title="Title" scroll>
-            <NavegationBar/>
+          <Header className="header-color" title="Title" scroll>
+            <NavegationBar />
           </Header>
           <Drawer title="Title">
             <Navigation>
-            <Link to="/aboutme">About Me</Link>
-              <Link to="/resume">Resume</Link>
-              <Link to="/Projects">Projects</Link>
-              <Link to="/contact">Contact</Link>
+              <NavegationBar/>
             </Navigation>
           </Drawer>
           <Content>
             <div className="page-content" />
-            <Main/>
+            <Main />
           </Content>
         </Layout>
       </div>
