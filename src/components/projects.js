299f62b4201fd6f30204b4f8cbcaebbedfc9791e
davidtrigo <d.trigo83@gmail.com>
2020-11-25 12:55:14 +0100

    add card  angular

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,62 +9,88 @@ class Projects extends Component {
     toggleCategories(){
         if(this.state.activeTab===0){
          return(
-             <div  className="projects-grid">
-            {/*Project 1*/}
-            <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
-                <CardTitle style={{color:'#fff',height:'176px',
-                background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
-                React project #1</CardTitle>
-                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Codepen</Button>
-                    <Button colored>Live Demo</Button>
-                </CardActions>
-                <CardMenu style={{color:'#fff'}}>
-                    <IconButton name="share"/>
-                </CardMenu>
-            </Card>
+            <div  className="projects-grid">
+                    {/*Project 1*/}
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
+                        <CardTitle style={{color:'#fff',height:'176px',
+                        background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
+                        React project #1</CardTitle>
+                        <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>Codepen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{color:'#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+                    </Card>
 
-             {/*Project 2*/}
-             <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
-                <CardTitle style={{color:'#fff',height:'176px',
-                background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
-                React project #2</CardTitle>
-                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Codepen</Button>
-                    <Button colored>Live Demo</Button>
-                </CardActions>
-                <CardMenu style={{color:'#fff'}}>
-                    <IconButton name="share"/>
-                </CardMenu>
-            </Card>
+                    {/*Project 2*/}
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
+                    <CardTitle style={{color:'#fff',height:'176px',
+                    background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
+                    React project #2</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>Codepen</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
 
 
-             {/*Project 3*/}
-             <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
-                <CardTitle style={{color:'#fff',height:'176px',
-                background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
-                React project #3</CardTitle>
-                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
-                <CardActions border>
-                    <Button colored>Github</Button>
-                    <Button colored>Codepen</Button>
-                    <Button colored>Live Demo</Button>
-                </CardActions>
-                <CardMenu style={{color:'#fff'}}>
-                    <IconButton name="share"/>
-                </CardMenu>
-            </Card>
+                {/*Project 3*/}
+                <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
+                    <CardTitle style={{color:'#fff',height:'176px',
+                    background:'url(https://miro.medium.com/max/1200/1*jnqXL4Q-iW0qxodFDTxyFQ.jpeg) center/cover'}}>
+                    React project #3</CardTitle>
+                    <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                        <Button colored>Codepen</Button>
+                        <Button colored>Live Demo</Button>
+                    </CardActions>
+                    <CardMenu style={{color:'#fff'}}>
+                        <IconButton name="share"/>
+                    </CardMenu>
+                </Card>
 
-             </div>
+            </div>
            
          )
         }
         else if(this.state.activeTab===1){
-            return(<div><h1>THIS IS ANGULAR</h1></div>)
+            return(
+
+<div  className="projects-grid">
+                    {/*Project 1*/}
+                    <Card shadow={5} style={{minWidth:'450', margin:'auto'}}> 
+                        <CardTitle style={{color:'#fff',height:'176px',
+                        background:'url(https://desarrolloweb.com/storage/manual_images/Ob3Wz6y0TnJCyccfIdXn1sdD7HWevyrbSyIxICHP.jpeg)'}}>
+                        Angular project #1</CardTitle>
+                        <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</CardText>
+                        <CardActions border>
+                            <Button colored>Github</Button>
+                            <Button colored>Codepen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{color:'#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+                    </Card>
+
+                
+
+
+               
+
+            </div>
+
+            )
            }
         else if(this.state.activeTab===2){
         return(<div><h1>THIS IS VUEJS</h1></div>)
